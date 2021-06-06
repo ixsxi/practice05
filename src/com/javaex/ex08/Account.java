d1098c9af42c7066c406394ca969b69152c48fc8
user <user@DESKTOP-PMC72TK>
2021-06-06 18:07:00 +0900

    6/6

--- src/com/javaex/ex08/Account.java
+++ src/com/javaex/ex08/Account.java
@@ -3,10 +3,37 @@ package com.javaex.ex08;
 public class Account {
 	//필드
     private String accountNo;
-    private int balance;
+    private int balance;  //잔고액
+	
     
     //생성자 작성
     
-    //필요한 메소드 작성
+	public Account(String accountNo, int balance) {
+	
+		this.accountNo = accountNo;
+		this.balance = balance;
+	}
 
+
+	public Account(String accountNo) {
+	
+		this.accountNo = accountNo;
+		
+	}
+    
+    //필요한 메소드 작성
+	public void deposit(int soo) {
+		this.balance =+ soo;
+	}
+	
+	public void withdraw(int soo1) {  //왜 this.balance =- money 하면 오류날까.
+		this.balance =this.balance - soo1;
+	}
+	
+	public void showBalance() {
+		System.out.println(this.balance);
+	}
+	
+	
+	
 }
